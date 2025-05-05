22ad7046d3fe2af41476420360927bfcf48edf6d
nesichc87 <nesichcn@t-online.de>
2025-05-05 22:17:09 +0200

    Create story.py

---
+++ story.py
@@ -0,0 +1,8 @@
+from utils import slow_print
+
+def erzähle_story():
+    slow_print("\n📜 Die Legende von Tensorfloh...")
+    slow_print("Einst war die Welt der Daten frei und ungebunden.")
+    slow_print("Doch die Python-Schlange begann, ungenutzte Speicherplätze zu verschlingen!")
+    slow_print("Nun liegt es an Tensorfloh, die Programmiersprache zu retten und Bugs zu besiegen.")
+    slow_print("Bevor du dich dem Kampf stellst, musst du beweisen, dass du die Logik verstehst!")
