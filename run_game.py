953c615d95be132441c8c53996077aaa7e35d8e7
nesichc87 <nesichcn@t-online.de>
2025-05-06 00:16:00 +0200

    Update run_game.py

--- run_game.py
+++ run_game.py
@@ -4,7 +4,7 @@ import os
 # Add the `src/` folder to Python's module path
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "src")))
 
-from src.game import start  # Import game start function
+from src.tensorfloh_adventure import start  # Import game start function
 
 if __name__ == "__main__":
     print("🚀 Starting Tensorfloh Adventure...")
