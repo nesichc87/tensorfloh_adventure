fc0f84b9b3d5e1afc26bd15685150195cf3fc730
nesichc87 <nesichcn@t-online.de>
2025-05-06 00:02:35 +0200

    Delete utils.py

--- utils.py
+++
@@ -1,8 +0,0 @@
-import time
-
-def slow_print(text):
-    """Gibt Text langsam aus, um die Atmosphäre zu verstärken."""
-    for char in text:
-        print(char, end='', flush=True)
-        time.sleep(0.03)
-    print()
